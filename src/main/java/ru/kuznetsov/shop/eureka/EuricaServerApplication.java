1b6d33e53f752a35ea441307602f3efd81bbf5cb
Pavel Kuznetsov <Technic47@gmail.com>
2026-01-15 16:55:04 +0300

    Final set up.

--- src/main/java/ru/kuznetsov/shop/eureka/EuricaServerApplication.java
+++ src/main/java/ru/kuznetsov/shop/eureka/EuricaServerApplication.java
@@ -1,9 +1,11 @@
-package ru.kuznetsov.shop.eurica;
+package ru.kuznetsov.shop.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EuricaServerApplication {
 
     public static void main(String[] args) {
--- src/test/java/ru/kuznetsov/shop/eureka/EuricaServerApplicationTests.java
+++ src/test/java/ru/kuznetsov/shop/eureka/EuricaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package ru.kuznetsov.shop.eurica;
+package ru.kuznetsov.shop.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
