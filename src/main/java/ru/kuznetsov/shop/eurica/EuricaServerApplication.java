9ff486c94e935bc0cd3f36a3ec96f0f5b600d2cd
Pavel Kuznetsov <Technic47@gmail.com>
2026-01-15 16:36:12 +0300

    Init commit.

---
+++ src/main/java/ru/kuznetsov/shop/eurica/EuricaServerApplication.java
@@ -0,0 +1,13 @@
+package ru.kuznetsov.shop.eurica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuricaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EuricaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kuznetsov/shop/eurica/EuricaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuznetsov.shop.eurica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuricaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
